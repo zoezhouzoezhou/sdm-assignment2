b9ca1282cc5075088d0796736fb34a74e4713e95
Mao Chuan Li <mao.chuan.li@gmail.com>
2015-08-20 22:01:41 +1200

    add 2 dummy test in test/models/dummy_test.rb file
    a new dummy test file for test travis CI server
    Signed-off-by: Mao Chuan Li <mao.chuan.li@gmail.com>

---
+++ test/models/dummy_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+ 
+class DummyTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+
+  test "another truth" do
+       assert 1==1
+  end
+end
