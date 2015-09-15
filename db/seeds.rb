32d04119d567f108b0289d558d5e02dfff2c90b4
Mao Chuan Li <mao@localhost.localdomain>
2015-09-16 10:50:28 +1200

    create 4 default users with 4 different roles for dev/test
    admin/moderator/analyst/regular@serler.com all with password:
    'password'. Users may also register new users from the sign up
    web page.
    
    Signed-off-by: Mao Chuan Li <mao.chuan.li@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.new({ :email => "admin@serler.com", :password => "password", :password_confirmation=>"password", :role=>"admin"}).save
+User.new({ :email => "moderator@serler.com", :password => "password", :password_confirmation=>"password", :role=>"moderator"}).save
+User.new({ :email => "analyst@serler.com", :password => "password", :password_confirmation=>"password", :role=>"analyst"}).save
+User.new({ :email => "regular@serler.com", :password => "password", :password_confirmation=>"password", :role=>"regular"}).save
+puts "created 4 default users: admin/moderator/analyst/regular with password: 'password'"
